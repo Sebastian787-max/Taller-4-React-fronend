ec0d58da4a533d0e9cf3c6e0c20227cacc99042c
Sebastian787-max <ganzua787@gmail.com>
2026-04-13 11:27:32 -0500

    Update axios.js

--- src/features/auth/api/axios.js
+++ src/features/auth/api/axios.js
@@ -1,18 +1,15 @@
 // src/api/axios.js
 import axios from 'axios';
-/**
-* Cliente HTTP centralizado:
-* - baseURL apunta al backend
-* - headers comunes
-*/
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+    baseURL: 'https://taller-4-react-backend-7.onrender.com/api',
     headers: { 'Content-Type': 'application/json' }
 });
-// Interceptor para adjuntar el token si existe:
+
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
+
 export default api;
